426d63be7a9910c123b01ec5267dc10e224be78a
Jesús Valero <jvalerodev@gmail.com>
2022-11-05 15:51:20 -0400

    compute_energy method solved

--- lib/src/Energy.cpp
+++ lib/src/Energy.cpp
@@ -1,22 +1,79 @@
+#include <iostream>
 #include <cmath>
 
 #include <Color.hpp>
 #include <Energy.hpp>
 
-Energy::Energy(const std::vector<std::vector<Color>>& _pixels) noexcept
-  : pixels(_pixels)
+Energy::Energy(const std::vector<std::vector<Color>> &_pixels) noexcept
+    : pixels(_pixels)
 {
-
 }
 
 std::vector<std::vector<unsigned>> Energy::compute_energy() const noexcept
 {
-  // TODO
+  int height = pixels.size();
+  int width = pixels[0].size();
+  std::vector<std::vector<unsigned>> energy_matrix(height, std::vector<unsigned>(width, 0));
 
-  return std::vector<std::vector<unsigned>>{};
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = 0; j < width; j++)
+    {
+      int rx = 0, gx = 0, bx = 0;
+      int ry = 0, gy = 0, by = 0;
+
+      // X gradient
+      if (j == 0) // Left of the image
+      {
+        rx = pixels[i][j].get_red() - pixels[i][j + 1].get_red();
+        gx = pixels[i][j].get_green() - pixels[i][j + 1].get_green();
+        bx = pixels[i][j].get_blue() - pixels[i][j + 1].get_blue();
+      }
+      else if (j == width - 1) // Rigth of the image
+      {
+        rx = pixels[i][j].get_red() - pixels[i][j - 1].get_red();
+        gx = pixels[i][j].get_green() - pixels[i][j - 1].get_green();
+        bx = pixels[i][j].get_blue() - pixels[i][j - 1].get_blue();
+      }
+      else
+      {
+        rx = pixels[i][j - 1].get_red() - pixels[i][j + 1].get_red();
+        gx = pixels[i][j - 1].get_green() - pixels[i][j + 1].get_green();
+        bx = pixels[i][j - 1].get_blue() - pixels[i][j + 1].get_blue();
+      }
+
+      // Y gradient
+      if (i == 0) // Top of the image
+      {
+        ry = pixels[i][j].get_red() - pixels[i + 1][j].get_red();
+        gy = pixels[i][j].get_green() - pixels[i + 1][j].get_green();
+        by = pixels[i][j].get_blue() - pixels[i + 1][j].get_blue();
+      }
+      else if (i == height - 1) // Bottom of the image
+      {
+        ry = pixels[i][j].get_red() - pixels[i - 1][j].get_red();
+        gy = pixels[i][j].get_green() - pixels[i - 1][j].get_green();
+        by = pixels[i][j].get_blue() - pixels[i - 1][j].get_blue();
+      }
+      else
+      {
+        ry = pixels[i - 1][j].get_red() - pixels[i + 1][j].get_red();
+        gy = pixels[i - 1][j].get_green() - pixels[i + 1][j].get_green();
+        by = pixels[i - 1][j].get_blue() - pixels[i + 1][j].get_blue();
+      }
+
+      float x_grad = sqrt(pow(rx, 2) + pow(gx, 2) + pow(bx, 2));
+      float y_grad = sqrt(pow(ry, 2) + pow(gy, 2) + pow(by, 2));
+      unsigned energy = x_grad + y_grad;
+
+      energy_matrix[i][j] = energy;
+    }
+  }
+
+  return energy_matrix;
 }
 
-std::vector<std::vector<Color>> Energy::energy_to_colors(const std::vector<std::vector<unsigned>>& energy)
+std::vector<std::vector<Color>> Energy::energy_to_colors(const std::vector<std::vector<unsigned>> &energy)
 {
   std::vector<std::vector<Color>> colors{energy.size(), std::vector<Color>{}};
 
@@ -24,7 +81,7 @@ std::vector<std::vector<Color>> Energy::energy_to_colors(const std::vector<std::
 
   for (std::size_t i = 0; i < energy.size(); ++i)
   {
-    for (unsigned e: energy[i])
+    for (unsigned e : energy[i])
     {
       if (e > max_energy)
       {
@@ -35,7 +92,7 @@ std::vector<std::vector<Color>> Energy::energy_to_colors(const std::vector<std::
 
   for (std::size_t i = 0; i < energy.size(); ++i)
   {
-    for (unsigned e: energy[i])
+    for (unsigned e : energy[i])
     {
       // Normalize the energy value to be in [0, 255]
       uint8_t energy_normalized = round(255.f * float(e) / float(max_energy));
