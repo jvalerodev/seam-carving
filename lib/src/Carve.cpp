35fa5a4227a2efdf3ee56f891968d8985914df58
Jesús Valero <jvalerodev@gmail.com>
2022-11-07 00:41:47 -0400

    remove_seam method solved

--- lib/src/Carve.cpp
+++ lib/src/Carve.cpp
@@ -3,22 +3,42 @@
 #include <Energy.hpp>
 #include <Seam.hpp>
 
-Carve::Carve(const std::vector<std::vector<Color>>& _pixels) noexcept
-  : pixels(_pixels)
+Carve::Carve(const std::vector<std::vector<Color>> &_pixels) noexcept
+    : pixels(_pixels)
 {
-  
 }
 
 std::vector<std::vector<Color>> Carve::remove_n_lowest_seams(std::size_t num_seams_to_remove) const noexcept
 {
-  // TODO
+  std::vector<std::vector<Color>> new_image(pixels);
 
-  return std::vector<std::vector<Color>>{};
+  for (int num = 0; num < num_seams_to_remove; num++)
+  {
+    std::vector<std::vector<unsigned>> energy_matrix = Energy(new_image).compute_energy();
+    std::vector<std::size_t> optimal_path = Seam(energy_matrix).compute_vertical_seams().first;
+    new_image = remove_seam(new_image, optimal_path);
+  }
+
+  return new_image;
 }
 
-std::vector<std::vector<Color>> Carve::remove_seam(const std::vector<std::vector<Color>>& image, const std::vector<std::size_t>& seam_cols) const noexcept
+std::vector<std::vector<Color>> Carve::remove_seam(const std::vector<std::vector<Color>> &image, const std::vector<std::size_t> &seam_cols) const noexcept
 {
-  // TODO
+  long height = image.size();
+  long width = image[0].size();
 
-  return std::vector<std::vector<Color>>{};
+  std::vector<std::vector<Color>> new_image(height);
+
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = 0; j < width; j++)
+    {
+      if (j != seam_cols[i])
+      {
+        new_image[i].push_back(image[i][j]);
+      }
+    }
+  }
+
+  return new_image;
 }
